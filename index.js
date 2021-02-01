dfc5d908f212c222a0e9021298cc45e154bca345
Shilcof <robert.shilcof@gmail.com>
2021-02-01 11:49:12 +0000

    commit tree 445cc20f554b66477723c874994540263efd7843

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution here
+
+
+function findMatching(list, string){
+    return list.filter(item => 
+        item.toLowerCase() === string.toLowerCase()
+    );
+}
+
+    // 1) returns all drivers that match the passed in name
+    // 2) returns matching drivers if case does not match but letters do
+    // 3) returns an empty array if there is no match
+
+
+function fuzzyMatch(list, string) {
+    return list.filter(item => 
+        !!item.match("^" + string)
+    );
+}
+
+    // 4) returns a driver if beginning provided letters match
+    // 5) does not return drivers if only middle or ending letters match
+    // 6) does not return drivers if only middle or ending letters match
+
+
+function matchName(list, string) {
+    return list.filter(item => 
+        item.name === string
+    );
+}
+
+    // 7) accesses the data structure to check if name matches
